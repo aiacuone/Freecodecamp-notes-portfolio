dc88001697cc602ef3561b5f72d0e4d387437e14
aiacuone <aiacuone@users.noreply.github.com>
2021-04-25 16:00:23 +0100

    intro text change

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,12 +6,8 @@ export default function Intro({ state, setState }) {
 		<div class="intro">
 			<img class="intro_logo" src={logo} />
 			<p class="intro_paragraph">
-				Welcome to my Freecodecamp Notes! <br />
-				<br />
-				The purpose of this app is not to wow you with creativity, but to give
-				you an idea of the work that ive done to progress to where I am today.
-				<br />
-				<br />I have kept all my notes, feel free to explore and view how I
+				Welcome to my Freecodecamp Online Course Notes. <br />
+				<br />Feel free to explore and view how I
 				approached certain problems by clicking Continue!
 			</p>
 			<p class="intro_continue" onClick={() => setState.setDirectory([])}>
